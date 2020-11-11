e0b08edfd06b22183d666ea6715b38d921ee842d
root <root@ba49c3985edc>
2020-11-11 02:25:47 +0000

    Finished goofynames and snowflake

--- src/_01_algorithms/_3_goofy_names/GoofyNames.java
+++ src/_01_algorithms/_3_goofy_names/GoofyNames.java
@@ -11,27 +11,37 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-JOptionPane.showInputDialog("Whats your name ");
-//String i ;
+		String goofy = JOptionPane.showInputDialog("Whats your name ");
+
+		// String i ;
 		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
+		// Run your program to see that this works.
+		System.out.println(goofy.toUpperCase());
 
 		// 3. Loop through each character of the name (steps 4 - 6).
-		//    HINT: Use .length() to determine the number of characters in the String.
+		// HINT: Use .length() to determine the number of characters in the String.
+		for (int i = 0; i < goofy.length(); i++) {
+			String u = goofy.substring(i, i + 1);
+			System.out.println(u);
+			if (i % 2 == 0) {
+				u = u.toUpperCase();
 
-			// 4. Create a String variable to store the next character of the name
-			//    using .substring(start, end)
-			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
-			// 5. Use MODULO to set this variable to upper case for EVEN characters
-			//    and lower case for ODD characters.
-
-			
-			// 6. ADD this String  (containing 1 char) to the goofyName String
-
-		
-		// 7. Use pop-up to show user their Goofy name
-
+			} else {
+				u = u.toLowerCase();
+			}
+			goofyName += u;
+		}
+		JOptionPane.showMessageDialog(null,goofyName );
 	}
-}
+	// 4. Create a String variable to store the next character of the name
+	// using .substring(start, end)
+	// HINT: replace 'start' and 'end' to get String with character at i
 
+	// 5. Use MODULO to set this variable to upper case for EVEN characters
+	// and lower case for ODD characters.
+
+	// 6. ADD this String (containing 1 char) to the goofyName String
+
+	// 7. Use pop-up to show user their Goofy name
+
+}
