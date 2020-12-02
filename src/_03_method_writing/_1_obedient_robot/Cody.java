08db6e85287e8c840ebe259b70b9f5fe576f109b
root <root@34f19c272b89>
2020-12-02 02:29:01 +0000

    Yo

--- src/_03_method_writing/_1_obedient_robot/Cody.java
+++ src/_03_method_writing/_1_obedient_robot/Cody.java
@@ -1,7 +1,44 @@
 package _03_method_writing._1_obedient_robot;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class Cody {
-public static void main(String[] args) {
+
+	public static void main(String[] args) {
+
+		Robot rob = new Robot();
+		
+		Cody.drawSquare(rob);
+		Cody.drawTriangle(rob);
+		
+
+	}
 	
-}
+	public static void drawSquare(Robot rob)
+	{
+		rob.penDown();
+		rob.turn(90);
+		rob.move(100);
+		rob.turn(90);
+		rob.move(100);
+		rob.turn(90);
+		rob.move(100);
+		rob.turn(90);
+		rob.move(100);
+	}
+	
+	public static void drawTriangle(Robot rob)
+	{
+		//All Triangles have 180 degrees! hint!
+		rob.move(150);
+		rob.turn(90);
+		rob.turn(90);
+		rob.move(150);
+		rob.turn(90);
+		rob.move(50);
+		
+	}
+
 }
