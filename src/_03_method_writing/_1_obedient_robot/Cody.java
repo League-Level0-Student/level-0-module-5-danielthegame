249a1fbb5cddf59873fdbef21362a93ec595db6b
root <root@21d4e6d121ab>
2020-12-16 02:22:22 +0000

    Finished obedient robot

--- src/_03_method_writing/_1_obedient_robot/Cody.java
+++ src/_03_method_writing/_1_obedient_robot/Cody.java
@@ -16,36 +16,60 @@ public class Cody {
 		//Cody.drawTriangle(rob);
 		
 String shape=JOptionPane.showInputDialog("Pick a shape");
+String color  =JOptionPane.showInputDialog("Pick a color");
+if(color.equals("red")) {
+	rob.setPenColor(Color.red);
+}
+if(color.equals("yellow")) {
+	rob.setPenColor(Color.yellow);
+}
+	if(color.equals("blue")) {
+		rob.setPenColor(Color.blue);
+	}
+rob.penDown();
+rob.setSpeed(20);
 if(shape.equals("square")) {
 	drawSquare(rob);
+	
 }
+else if(shape.equals("circle"))
+	drawCircle(rob);
 	  else {
+		  
 		  drawTriangle(rob);
+		  
 	  }
 
 	}
 
 	public static void drawSquare(Robot rob)
 	{
-		rob.penDown();
-		rob.turn(90);
-		rob.move(100);
-		rob.turn(90);
-		rob.move(100);
-		rob.turn(90);
-		rob.move(100);
-		rob.turn(90);
-		rob.move(100);
+		for (int i = 0; i < 4; i++) {
+			rob.turn(90);
+			rob.move(100);
+		}
 	}
 	
 	public static void drawTriangle(Robot rob)
 	{
 		//All Triangles have 180 degrees! hint!
-		rob.turn(30);
-		rob.move(100);
-		rob.turn(120);
-		rob.move(100);
+		
+		for (int i = 0; i < 3; i++) {
+			rob.turn(120);
+			rob.move(100);
+		}
+		//360 / numberofSides
+		
+			
 		
 	}
-
+public static void drawCircle(Robot rob ) {
+	
+	for (int i = 0; i < 360; i++) {
+		rob.turn(1);
+		rob.move(1);
+	}
+	
+	
+}
 }
